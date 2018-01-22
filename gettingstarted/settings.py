d1602d44661c81ac4b4e87ade94bf443f1c85e49
ankush <ankush_gumber@codenomad.net>
2018-01-22 20:37:45 +0530

    chnages

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -156,7 +156,7 @@ LOGGING = {
 MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'media')
 MEDIA_URL = '/media/'
 
-try:
-    from .local_settings import *
-except ImportError:
-    pass
+# try:
+    # from .local_settings import *
+# except ImportError:
+    # pass
