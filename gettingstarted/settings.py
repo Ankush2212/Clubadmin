7de1eef003f9ceadebba326cf66d067d285cc434
ankush <ankush_gumber@codenomad.net>
2018-01-23 22:33:22 +0530

    changes

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -78,19 +78,28 @@ WSGI_APPLICATION = 'gettingstarted.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
+# DATABASES = {
+    # 'default': {
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+
+    # },
+   # 'OPTIONS': {
+        # 'timeout': 50,
+    # }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-
-    },
-   'OPTIONS': {
-        'timeout': 50,
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        
     }
 }
 
 # add this
-
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
+#DATABASES['default']['ENGINE'] = 'django.db.backends.postgresql_psycopg2'
+DATABASES['default']['CONN_MAX_AGE'] = 500
 # db_from_env = dj_database_url.config()
 # DATABASES['default'].update(db_from_env)
 # DATABASES['default']['ENGINE'] = 'django.db.backends.postgresql_psycopg2'
