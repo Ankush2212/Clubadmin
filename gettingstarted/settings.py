4c2896e02fa56292e488ab9b56e0678b3d36d9ff
ankush <ankush_gumber@codenomad.net>
2018-01-23 14:56:18 +0530

    done

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -78,21 +78,24 @@ WSGI_APPLICATION = 'gettingstarted.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    },
-   'OPTIONS': {
-        'timeout': 50,
-    }
-}
+# DATABASES = {
+    # 'default': {
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # },
+   # 'OPTIONS': {
+        # 'timeout': 50,
+    # }
+# }
+#DATABASES['default']['ENGINE'] = 'django.db.backends.postgresql_psycopg2'
 
 # add this
 
 db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
+DATABASES['default']['ENGINE'] = 'django.db.backends.postgresql_psycopg2'
 DATABASES['default']['CONN_MAX_AGE'] = 500
+
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
 # Honor the 'X-Forwarded-Proto' header for request.is_secure() 
