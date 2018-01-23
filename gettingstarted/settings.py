b3f6de4a7885b178d887615667b0d0d4535a21cc
ankush <ankush_gumber@codenomad.net>
2018-01-23 20:33:34 +0530

    Changes in setting file

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -78,15 +78,15 @@ WSGI_APPLICATION = 'gettingstarted.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-# DATABASES = {
-    # 'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    # },
-   # 'OPTIONS': {
-        # 'timeout': 50,
-    # }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    },
+   'OPTIONS': {
+        'timeout': 50,
+    }
+}
 
 # add this
 
