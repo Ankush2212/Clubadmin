a4d0d9982061843892cdd6ab8e55772f96638b98
ankush <ankush_gumber@codenomad.net>
2018-01-24 12:43:17 +0530

    done

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 import os
 import django_heroku
 import dj_database_url
+from urllib import parse
+import psycopg2
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -89,7 +91,7 @@ DATABASES = {
  'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'test',  # Or path to database file if using sqlite3.
-        'USER': '',  # Not used with sqlite3.
+        'USER': 'postgres',  # Not used with sqlite3.
         'PASSWORD': '',  # Not used with sqlite3.
         'HOST': '',  # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',  # Set to empty string for default. Not used with sqlite3. 
