ca64455829302a73c3507abb66656523a2a00732
ankush <ankush_gumber@codenomad.net>
2018-01-24 13:06:57 +0530

    done

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -102,11 +102,11 @@ DATABASES = {
     'default': dj_database_url.config()
 }
 
-# db_from_env = dj_database_url.config(conn_max_age=600)
-# DATABASES['default'].update(db_from_env)
-# DATABASES['default'] =  dj_database_url.config()
-# DATABASES['default']['ENGINE'] = 'django.db.backends.postgresql_psycopg2'
-# DATABASES['default']['NAME'] = 'test'
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
+DATABASES['default'] =  dj_database_url.config()
+DATABASES['default']['ENGINE'] = 'django.db.backends.postgresql_psycopg2'
+DATABASES['default']['NAME'] = 'test'
 #DATABASES['default']['CONN_MAX_AGE'] = 500
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
