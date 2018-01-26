7ddf06222ebc8cab5eff204dd47827ce249cb85d
Ankush2212 <ankush_gumber@codenomad.net>
2018-01-25 19:21:56 -0800

    dfds

--- hello/views.py
+++ hello/views.py
@@ -250,8 +250,8 @@ def addemployee(request):
 				contactnumber = request.POST.get('contactnumber')
 				now = datetime.datetime.now()
 				#print(request.POST)
-				return HttpResponse(employeename)
-				data = adduser(employeename=employeename,contactnumber=contactnumber,username=username,dateofjoin=now,email=email)
+				#return HttpResponse(employeename)
+				data = addemployee(employeename=employeename,contactnumber=contactnumber,username=username,dateofjoin=now,email=email)
 				data.save()
 				error = "New employee added successfully"
 				return render(request, 'backend/addnewemployee.html',{'getadta':getrecord,'success':error})
