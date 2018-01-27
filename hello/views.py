33235b737689a0d9fc6a044701d8b554f9a253c7
Ankush2212 <ankush_gumber@codenomad.net>
2018-01-27 15:52:52 -0800

    fdf
    d

--- hello/views.py
+++ hello/views.py
@@ -291,7 +291,8 @@ def deleteemployee(request):
 			getrecord1 =  adminsignup.objects.get(id=getsession)
 			getemployeedetails =  employeedetail.objects.all()
 			successer="Employee deleted successfully."
-			return redirect(request, 'backend/getemployee.html',{'getadta':getrecord1,'getemployeedetail':getemployeedetails,'success':successer})
+			return render(request, 'backend/getemployee.html',{'getadta':getrecord1,'getemployeedetail':getemployeedetails,'success':successer})
+			return redirect(getemployee)
 		except KeyError:
 				#return HttpResponse(str(e))
 				error="Due to error employee not deleted."
