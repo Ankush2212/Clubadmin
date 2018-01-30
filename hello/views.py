2bc85e34cf657d2e1d6deb225e77d5b8f0b202f1
Ankush2212 <ankush_gumber@codenomad.net>
2018-01-30 12:48:35 -0800

    changes

--- hello/views.py
+++ hello/views.py
@@ -1,10 +1,3 @@
-try:
-    # Fix UTF8 output issues on Windows console.
-    # Does nothing if package is not installed
-    from win_unicode_console import enable
-    enable()
-except ImportError:
-    pass
 import requests
 
 from django.contrib import messages
