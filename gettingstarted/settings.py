35277354a4dc3e0232308cd162f12e2c27216883
Ankush2212 <ankush_gumber@codenomad.net>
2018-01-30 18:13:27 -0800

    fdgdf

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -196,7 +196,7 @@ MEDIA_URL = '/media/'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 465
+EMAIL_PORT = 587
 EMAIL_HOST_USER = 'developerweb6@gmail.com'
 EMAIL_HOST_PASSWORD = 'codenomad@8804'
 
