b6ab38c0202d3972d36303224a5a1f380e952960
Ankush2212 <ankush_gumber@codenomad.net>
2018-01-30 19:11:42 -0800

    SMTP

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -199,7 +199,7 @@ MEDIA_URL = '/media/'
 
 
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_PASSWORD = 'codenomad@8804'
+EMAIL_HOST_PASSWORD = 'strike@987'
 EMAIL_HOST_USER = 'developerweb6@gmail.com'
 EMAIL_PORT = 465
 EMAIL_USE_TLS = True
