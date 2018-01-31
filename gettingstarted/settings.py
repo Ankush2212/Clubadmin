cec909c6da5defcebf551682cdce9eb21c9b258d
Ankush2212 <ankush_gumber@codenomad.net>
2018-01-30 19:40:45 -0800

    Credentials

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -199,8 +199,8 @@ MEDIA_URL = '/media/'
 
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'developerweb6@gmail.com'
-EMAIL_HOST_PASSWORD = 'strike@987'
+EMAIL_HOST_USER = 'ankush_gumber@codenomad.net'
+EMAIL_HOST_PASSWORD = 'farewell241'
 EMAIL_PORT = 465
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 #smtp changes
