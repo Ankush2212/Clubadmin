112564d3064f9e9c9c3b34480995fc5fb851fc0d
Ankush2212 <ankush_gumber@codenomad.net>
2018-01-30 19:39:41 -0800

    SMTP

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -199,9 +199,9 @@ MEDIA_URL = '/media/'
 
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'support@deplerai.com'
-EMAIL_HOST_PASSWORD = 'Support2017'
-EMAIL_PORT = 3535
+EMAIL_HOST_USER = 'developerweb6@gmail.com'
+EMAIL_HOST_PASSWORD = 'strike@987'
+EMAIL_PORT = 465
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 #smtp changes
 EMAIL_BACKEND = 'django_smtp_ssl.SSLEmailBackend'
