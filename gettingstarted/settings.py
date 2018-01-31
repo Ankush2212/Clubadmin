640f70cacf499591ea870f3333e400b0319c2f8a
Ankush2212 <ankush_gumber@codenomad.net>
2018-01-31 09:57:51 -0800

    mail

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -199,8 +199,8 @@ MEDIA_URL = '/media/'
 
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'ankush_gumber@codenomad.net'
-EMAIL_HOST_PASSWORD = 'farewell241'
+EMAIL_HOST_USER = 'kalpana@codenomad.net'
+EMAIL_HOST_PASSWORD = 'codenomad@123'
 EMAIL_PORT = 465
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 #smtp changes
