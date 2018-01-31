37137ae092ca0615c1d8132f41be7a09a73d9b3d
Ankush2212 <ankush_gumber@codenomad.net>
2018-01-30 18:40:38 -0800

    smtp settings 3

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -203,7 +203,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_PASSWORD = 'impankaj'
 EMAIL_HOST_USER = 'pankajsaharan9@gmail.com'
 EMAIL_PORT = 465
-EMAIL_USE_TLS = True
+EMAIL_USE_TLS = False
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 
