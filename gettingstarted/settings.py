21047a25fb0108f4322b38e99b98838a7c0754c8
Ankush2212 <ankush_gumber@codenomad.net>
2018-01-30 19:07:08 -0800

    SMTP

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -202,7 +202,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_PASSWORD = 'codenomad@8804'
 EMAIL_HOST_USER = 'developerweb6@gmail.com'
 EMAIL_PORT = 465
-EMAIL_USE_TLS = False
+EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 #smtp changes
 EMAIL_BACKEND = 'django_smtp_ssl.SSLEmailBackend'
