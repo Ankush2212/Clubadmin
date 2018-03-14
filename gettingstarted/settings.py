8735b25acb3a59ed6de338d968e14d44d631cad3
Ankush2212 <ankush_gumber@codenomad.net>
2018-03-14 11:10:19 +0530

    done

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -174,85 +174,24 @@ STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
 )
 django_heroku.settings(locals())
-# LOGGING = {
-    # 'version': 1,
-    # 'disable_existing_loggers': False,
-    # 'handlers': {
-        # 'file': {
-            # 'level': 'DEBUG',
-            # 'class': 'logging.FileHandler',
-            # 'filename': 'debug.log',
-        # },
-    # },
-    # 'loggers': {
-        # 'django': {
-            # 'handlers': ['file'],
-            # 'level': 'DEBUG',
-            # 'propagate': True,
-        # },
-    # },
-# }
-
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
     'handlers': {
-        'mail_admins': {
-            'level': 'ERROR',
-            'class': 'django.utils.log.AdminEmailHandler'
-        },
-        'null': {
-            'level':'DEBUG',
-            'class':'django.utils.log.NullHandler',
-        },
-        'console':{
-            'level':'DEBUG',
-            'class':'logging.StreamHandler',
-            'formatter': 'verbose'
-        },
-        'logfile': {
-            'level':'DEBUG',
-            'class':'logging.handlers.RotatingFileHandler',
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
             'filename': 'debug.log',
-            'maxBytes': 1024*1024*5, # 5MB
-            'backupCount': 0,
-            'formatter': 'verbose',
-        },
-    },
-    'formatters': {
-        'verbose': {
-            'format': '%(levelname)s|%(asctime)s|%(module)s|%(process)d|%(thread)d|%(message)s',
-            'datefmt' : "%d/%b/%Y %H:%M:%S"
-        },
-        'simple': {
-            'format': '%(levelname)s|%(message)s'
         },
     },
     'loggers': {
-        'myapp.request': {
-            'handlers': ['mail_admins'],
-            'level': 'ERROR',
-            'propagate': True,
-        },
-        'myapp.tasks': {
-            'handlers': ['mail_admins'],
-            'level': 'ERROR',
-            'propagate': True,
-        },
-        'myapp.management': {
-            'handlers': ['console', 'logfile'],
+        'django': {
+            'handlers': ['file'],
             'level': 'DEBUG',
             'propagate': True,
         },
-        'myapp.models': {
-            'handlers': ['console', 'logfile'],
-            'level': 'DEBUG',
-            'propagate': True,
-        },
-    }
+    },
 }
-
-
 
 
 MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'media')
