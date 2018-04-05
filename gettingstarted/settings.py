44cf93f11d48210ae8f8b051b62dbc22c68859c4
Ankush2212 <ankush_gumber@codenomad.net>
2018-04-05 16:27:27 +0530

    done

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -42,8 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'hello',
-	'requests'
+    'hello'
 ]
 PREPEND_WWW = True
 MIDDLEWARE = [
