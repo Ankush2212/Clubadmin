af6b9d0ad9a0cec65a377e322048c63c33863159
Ankush2212 <ankush_gumber@codenomad.net>
2018-04-16 15:12:35 +0530

    fgh

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -42,7 +42,8 @@ INSTALLED_APPS = [
 	'django.contrib.sessions',
 	'django.contrib.messages',
 	'django.contrib.staticfiles',
-	'hello'
+	'hello',
+	'intercom'
 ]
 PREPEND_WWW = True
 MIDDLEWARE = [
